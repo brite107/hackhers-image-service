b342e236038da5b571fcab3898ec6118d69bef39
Jen Perry <jperry@catalyte.io>
2021-04-25 09:03:38 -0500

    set port configuration in yml file

--- src/main/java/io/catalyte/training/hackhersimageservice/HackhersImageServiceApplication.java
+++ src/main/java/io/catalyte/training/hackhersimageservice/HackhersImageServiceApplication.java
@@ -8,10 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HackhersImageServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(HackhersImageServiceApplication.class);
-    app.setDefaultProperties(Collections
-        .singletonMap("server.port", "8081"));
-    app.run(args);
+    SpringApplication.run(HackhersImageServiceApplication.class, args);
   }
 
 }
